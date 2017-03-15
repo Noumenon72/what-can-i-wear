c470afb6a945f11838e979e90892435ebd24d619
Noumenon72 <kovaciny_karl@yahoo.com>
2017-03-14 21:11:04 -0400

    Revert "Setup for PythonAnywhere deploy"

---
+++ run.py
@@ -0,0 +1,7 @@
+from clothing import app
+
+# app.run(host='192.168.0.1', port=5000)
+# app.run(host='10.0.0.19', port=5000)
+# app.run(host='73.163.135.206', port=5000)
+# app.run(host='192.168.0.100', port=5000)
+app.run(host='localhost', port=5000)
